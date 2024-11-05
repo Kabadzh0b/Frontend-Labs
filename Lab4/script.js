aa104cf30880a5c0bdd92f34399388df1c545cf7
Maksym Chub <maksym.chub@develops.today>
2024-11-05 21:40:35 +0200

    feat: Lab 4 task 2

--- Lab4/script.js
+++ Lab4/script.js
@@ -23,3 +23,45 @@ myVariantElementByQuerySelector.addEventListener("click", () => {
   //   myVariantElementByQuerySelector.classList.toggle("new-color");
   //   myVariantElementByQuerySelector.classList.toggle("change-color");
 });
+
+const addButton = document.getElementById("add-button");
+const reduceButton = document.getElementById("reduce-button");
+const increaseButton = document.getElementById("increase-button");
+const deleteButton = document.getElementById("delete-button");
+
+const image = document.getElementById("image");
+let isImageVisible = true;
+let imageWidth = 700;
+
+// basic setup
+addButton.classList.add("disabled");
+image.style.width = imageWidth + "px";
+
+addButton.addEventListener("click", () => {
+  image.classList.remove("hide");
+  addButton.classList.add("disabled");
+  reduceButton.classList.remove("disabled");
+  increaseButton.classList.remove("disabled");
+  deleteButton.classList.remove("disabled");
+});
+
+reduceButton.addEventListener("click", () => {
+  imageWidth -= 100;
+  if (imageWidth < 100) {
+    imageWidth = 100;
+  }
+  image.style.width = imageWidth + "px";
+});
+
+increaseButton.addEventListener("click", () => {
+  imageWidth += 100;
+  image.style.width = imageWidth + "px";
+});
+
+deleteButton.addEventListener("click", () => {
+  image.classList.add("hide");
+  addButton.classList.remove("disabled");
+  reduceButton.classList.add("disabled");
+  increaseButton.classList.add("disabled");
+  deleteButton.classList.add("disabled");
+});
